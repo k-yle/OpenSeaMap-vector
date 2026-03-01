dc00445aedc5e13be25174fb3ec4f94e54c5b2da
Kyle Hensel <k.y.l.e@outlook.co.nz>
2026-03-01 22:43:04 +1100

    pin the version of osmium
    to avoid unexpected breaking changes

--- planet.sh
+++ planet.sh
@@ -8,7 +8,7 @@ echo "downloading the OSM planet file…"
 # full.pbf -> seamarks.pbf (if it doesn't exist)
 echo "running osmium tags-filter…"
 [ -f ./data/public/seamarks.pbf ] || docker run --rm -v $(pwd):/data \
-  iboates/osmium:latest \
+  iboates/osmium:1.19.0 \
   tags-filter \
   -e /data/data/osmium-tags-filter.ini \
   /data/data/full.pbf \
